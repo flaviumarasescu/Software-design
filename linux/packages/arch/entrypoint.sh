bd315290544d890ccfcc93c08a2f16d369bdf0f0
Flaviu Marasescu <50264537+flaviumarasescu@users.noreply.github.com>
2022-05-04 20:17:45 +0300

    Add files via upload

---
+++ linux/packages/arch/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cp /work_dir/*.tar.gz ./
+
+sources=$(ls audacity-sources-*)
+version=$(echo ${sources} | sed -e 's/audacity-sources-//g' -e 's/.tar.gz//g' -e 's/-/./g' -e 's/\.[0-9]\++/.r/g')
+
+echo "Audacity version: ${version}"
+
+buildLevel=2
+
+if [[ "${sources}" == *alpha* ]]; then
+    buildLevel=0
+elif [[ "${sources}" == *beta* ]]; then
+    buildLevel=1
+fi
+
+echo "Audacity build level: ${buildLevel}"
+
+deps=$(ls audacity-dependencies-*)
+
+sed -i -e "s|TMPL_AUDACITY_VERSION|${version}|g" PKGBUILD
+sed -i -e "s|TMPL_AUDACITY_SOURCES|${sources}|g" PKGBUILD
+sed -i -e "s|TMPL_AUDACITY_DEPENDENCIES|${deps}|g" PKGBUILD
+
+cat PKGBUILD
+
+makepkg
+
+ls -la /work_dir
+id
+
+cp *.zst /work_dir
